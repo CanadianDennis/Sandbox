e41b87aa84619ec0a648f2a120ab6e6d7b8e4601
Dennis McLean <dennis.mclean@gmail.com>
2016-02-23 00:49:49 -0500

    Added files via upload

--- Base_Scenario.py
+++ Base_Scenario.py
@@ -43,6 +43,7 @@ F = failure cumulative distribution function
 
 import random
 import math
+import statistics
 
 # Input parameters
 t_u = 1
@@ -51,7 +52,7 @@ tau_r = 0.5
 C_d = 6000
 C_p = 1000
 C_c = 5000
-h = 100
+h = 25
 C_o = 200
 C_h = 50
 nu_1 = 10
@@ -59,12 +60,12 @@ nu_2 = 70
 beta_1 = 3
 beta_2 = 3
 p_list = (0, 0.1, 0.3)
-N_list = (1, 2, 5)
+N_list = (1, 2, 5, 10, 15, 20, 30)
 
 # Temporary, to be removed and replaced with loops
-N = 5
+N = 1
 p = 0.3
-T = 51
+T = 46
 R = 1
 S = 1
 
@@ -81,47 +82,70 @@ def get_failure_time():
         i = round(t / t_u)
         if F[i] > random.random():
             return t
-    return h+1
+    return h+t_u
 
+n_i = 1
 
-# Compute initial times for preventive maintenance (t_p) and failures (t_f)
-t_p = []
-t_f = []
-for i in range(0, N):
-    t_p.append(T)
-    t_f.append(get_failure_time())
-t_r = R
+C_h_i = [0] * n_i
+C_p_i = [0] * n_i
+C_c_i = [0] * n_i
+C_d_i = [0] * n_i
+C_o_i = [0] * n_i
 
-S_o = S
-C = 0
-
-for t in range(t_u, h+t_u, t_u):
-    # Incur holding cost for previous time period
-    C += C_h * S_o
-
-    # Incur order cost if placing an order
-    if t == t_r:
-        C += C_o
-
-    # Receive spares after lead time has expired
-    if t == (t_r + tau):
-        S_o = S
-        t_r += R
-
+for n in range(0, n_i):
+    # Compute initial times for preventive maintenance (t_p) and failures (t_f)
+    t_p = []
+    t_f = []
     for i in range(0, N):
-        # Corrective maintenance is needed and can be performed
-        if t >= t_f[i] and S_o > 0:
-            S_o -= 1
-            C += C_c + C_d * (t - t_f[i]) / t_u
-            t_f[i] = t + get_failure_time()
-            t_p[i] = t + T
-        # Preventive maintenance is needed and can be performed
-        elif t >= t_p[i] and S_o > 0:
-            S_o -= 1
-            S_t -= 1
-            C += C_p
-            t_f[i] = t + get_failure_time()
-            t_p[i] = t + T
+        t_p.append(T)
+        t_f.append(get_failure_time())
+    t_r = R
 
-print("Result = ")
-print(C / (h / t_u))
+    print("t_p =", t_p)
+    print("t_f =", t_f)
+    print("t_r =", t_r)
+
+    S_o = S
+
+    for t in range(t_u, h+t_u, t_u):
+        # Incur holding cost for previous time period
+        C_h_i[n] += C_h * S_o
+
+        # Incur order cost if placing an order
+        if t == t_r:
+            C_o_i[n] += C_o
+
+        # Receive spares after lead time has expired
+        if t == (t_r + tau):
+            S_o = S
+            t_r += R
+
+        for i in range(0, N):
+            # Corrective maintenance is needed and can be performed
+            if t >= t_f[i] and S_o > 0:
+                S_o -= 1
+                C_c_i[n] += C_c
+                C_d_i[n] += C_d * (t - t_f[i]) / t_u
+                t_f[i] = t + get_failure_time()
+                t_p[i] = t + T
+            # Preventive maintenance is needed and can be performed
+            elif t >= t_p[i] and S_o > 0:
+                S_o -= 1
+                C_p_i[n] += C_p
+                t_f[i] = t + get_failure_time()
+                t_p[i] = t + T
+
+C_h_i = statistics.mean(C_h_i) / h
+C_p_i = statistics.mean(C_p_i) / h
+C_c_i = statistics.mean(C_c_i) / h
+C_d_i = statistics.mean(C_d_i) / h
+C_o_i = statistics.mean(C_o_i) / h
+C = C_h_i + C_p_i + C_c_i + C_d_i + C_o_i
+
+print("Results:")
+print("C     =", C)
+print("C_h_i =", C_h_i)
+print("C_p_i =", C_p_i)
+print("C_c_i =", C_c_i)
+print("C_d_i =", C_d_i)
+print("C_o_i =", C_o_i)
