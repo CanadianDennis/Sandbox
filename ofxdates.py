0820a14acb4b33c048a10f8391ad4fb03370ca06
Dennis McLean <dennis.mclean@gmail.com>
2017-07-04 21:56:16 -0400

    Add files via upload

--- ofxdates.py
+++ ofxdates.py
@@ -1,29 +1,35 @@
-from ofxparse import OfxParser
+from ofxparse import OfxParser, OfxPrinter
 import csv
 
-with open('statement.ofx') as ofxfile:
-    ofxdata = OfxParser.parse(ofxfile)
+def main():
 
-csvdata = []
-
-with open('statement.csv') as csvfile:
-    fieldnames = ['item_no', 'card_no', 'trans_date', 'post_date', 'amount', 'description']
-    csvreader = csv.DictReader(csvfile, fieldnames=fieldnames)
-    for csvtrans in csvreader:
-        csvdata.append(csvtrans)
-
-del(csvdata[0]) # Effective statement date
-del(csvdata[0]) # Column headings
-
-trans_count = len(csvdata)
-
-for i in range(trans_count):
-    csvtrans = csvdata[i]
-    ofxtrans = ofxdata.account.statement.transactions[i]
+    with open('statement.ofx') as ofxfile:
+        ofxdata = OfxParser.parse(ofxfile)
     
-    csvamount = -1.0 * float(csvtrans['amount'])
-    ofxamount = float(ofxtrans.amount)
+    csvdata = []
     
-    if csvamount != ofxamount:
-        print(csvamount, ofxamount)
+    with open('statement.csv') as csvfile:
+        fieldnames = ['item_no', 'card_no', 'trans_date', 'post_date', 'amount', 'description']
+        csvreader = csv.DictReader(csvfile, fieldnames=fieldnames)
+        for csvtrans in csvreader:
+            csvdata.append(csvtrans)
+    
+    del(csvdata[0]) # Effective statement date
+    del(csvdata[0]) # Column headings
+    
+    trans_count = len(csvdata)
+    
+    for i in range(trans_count):
         
+        csvamount = -1.0 * float(csvdata[i]['amount'])
+        ofxamount = float(ofxdata.account.statement.transactions[i].amount)
+        if csvamount != ofxamount:
+            print( 'Error: Transaction #%d does not match'% (i+1) )
+            return
+        
+        ofxprint = OfxPrinter(ofx=ofxdata, filename='output.ofx')
+        ofxprint.write()
+        
+if __name__ == '__main__':
+    main()
+    
